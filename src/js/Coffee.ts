96a6d3e5826fac761d7675774be39d2260f0c1de
DonCalzoneDK <58050674+DonCalzoneDK@users.noreply.github.com>
2020-10-07 11:15:45 +0200

    Task 1: created class Coffee, property discount, method price, method strength, and a constructor to set the discount property

---
+++ src/js/Coffee.ts
@@ -0,0 +1,18 @@
+/**Abstract Base Class Coffee */
+export abstract class Coffee{
+    /**Discount property for coffee of type number*/
+    public Discount: number;
+    /**Abstract Method Price() */
+    public abstract Price(): Number;
+    /**Method for Coffee Strength */
+    public Strength(): string{
+        return "Mild"
+    }
+    /**Constructor that takes parameter (discount:number) and sets
+     * the "discount" property
+     */
+    constructor (dicount:number){
+        this.Discount = dicount;
+    }
+
+}
